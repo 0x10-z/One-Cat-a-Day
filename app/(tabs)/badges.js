de0ae3d336a724d972248e3b8d343f73291bda35
iker ocio <iker@ikerocio.com>
2024-09-15 19:21:16 +0200

    badge animation added

--- app/(tabs)/badges.js
+++ app/(tabs)/badges.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import React, { useRef, useEffect } from "react";
+import { View, ScrollView, Text, Image, Animated } from "react-native";
 import { Screen } from "../../components/Screen";
 import { useBadgeContext } from "../../hooks/useBadgeContext"; // Importa el proveedor
 
@@ -12,21 +12,41 @@ function chunkArray(array, chunkSize) {
   return result;
 }
 
-const Badge = function Badge({ rowIndex, badgeRow }) {
+const Badge = function Badge({ badge, index }) {
+  const opacity = useRef(new Animated.Value(0.0)).current;
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 200,
+      delay: index * 200,
+      useNativeDriver: false,
+    }).start();
+  }, [opacity, index]);
+
+  return (
+    <Animated.View style={{ opacity }}>
+      <View key={badge.id} className="flex-1 items-center mx-2">
+        <View className="shadow-lg">
+          <Image
+            source={{
+              uri: `https://raw.githubusercontent.com/0x10-z/One-Cat-a-Day/master/${badge.image}`,
+            }}
+            className="w-28 h-28 rounded-lg"
+            resizeMode="contain"
+          />
+        </View>
+        <Text className="text-white mt-2 text-center">{badge.title}</Text>
+      </View>
+    </Animated.View>
+  );
+};
+
+const BadgeRow = function BadgeRow({ rowIndex, badgeRow }) {
   return (
     <View key={rowIndex} className="flex-row mb-6 justify-between">
-      {/* Recorremos cada medalla dentro de la fila */}
-      {badgeRow.map((badge) => (
-        <View key={badge.id} className="flex-1 items-center mx-2">
-          <View className="shadow-lg">
-            <Image
-              source={badge.image}
-              className="w-28 h-28 rounded-lg"
-              resizeMode="contain"
-            />
-          </View>
-          <Text className="text-white mt-2 text-center">{badge.title}</Text>
-        </View>
+      {badgeRow.map((badge, index) => (
+        <Badge key={badge.id} index={index} badge={badge} />
       ))}
       {/* Si la última fila tiene menos de 3 medallas, agregamos vistas vacías para mantener el diseño */}
       {badgeRow.length < 3 &&
@@ -47,10 +67,10 @@ export default function Badges() {
       <ScrollView>
         <View className="p-4">
           <Text className="text-white text-center font-bold mb-8 text-2xl">
-            Medallas {badgeList.length}
+            {badgeList.length} Medallas
           </Text>
           {badgeRows.map((row, rowIndex) => (
-            <Badge key={rowIndex} rowIndex={rowIndex} badgeRow={row} />
+            <BadgeRow key={rowIndex} rowIndex={rowIndex} badgeRow={row} />
           ))}
         </View>
       </ScrollView>
