72b4106fa7d9a1fb77789ca037c7096dc059f092
iker ocio <iker@ikerocio.com>
2024-09-15 15:11:05 +0200

    First cat obtained

--- app/(tabs)/cats.js
+++ app/(tabs)/cats.js
@@ -1,4 +1,4 @@
-import { FlatList } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { useEffect, useState } from "react";
 import { getCatImages } from "../../lib/onecataday";
 import { AnimatedCatCard } from "../../components/CatCard";
@@ -9,6 +9,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function MyCats() {
   const [cats, setCats] = useState([]);
   const [storedCatIds, setStoredCatIds] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado de carga
 
   // Carga los gatos almacenados desde AsyncStorage
   useEffect(() => {
@@ -38,6 +39,8 @@ export default function MyCats() {
         setCats(filteredCats);
       } catch (error) {
         console.error("Error al cargar los gatos:", error);
+      } finally {
+        setLoading(false); // Desactiva el estado de carga cuando se complete el fetch
       }
     };
 
@@ -46,9 +49,18 @@ export default function MyCats() {
 
   return (
     <Screen>
-      {cats.length === 0 ? (
+      {loading ? ( // Si está cargando, muestra el indicador
         <LoadingIndicator />
+      ) : cats.length === 0 ? ( // Si no hay gatos después de cargar
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text style={{ fontSize: 24, color: "#fff" }}>
+            ¡Obtén tu primer gato ya!
+          </Text>
+        </View>
       ) : (
+        // Muestra la lista si hay gatos
         <FlatList
           className="p-2"
           data={cats}
